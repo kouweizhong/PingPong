0879edfb5fb43729318ebd01a459ef03335a859c
Tarun Gupta <tarun.gupta@technogica.com>
2013-03-27 22:14:39 +0530

    Fixed Kilim maven weaver.

--- src/com/blockwithme/pingpong/latency/SmallLatencyBenchmarkTest.java
+++ src/com/blockwithme/pingpong/latency/SmallLatencyBenchmarkTest.java
@@ -15,10 +15,13 @@
  */
 package com.blockwithme.pingpong.latency;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import junit.framework.Assert;
 import kilim.Pausable;
+import kilim.Task;
 
 import org.agilewiki.jactor.JAMailboxFactory;
 import org.agilewiki.jactor.MailboxFactory;
@@ -47,6 +50,7 @@ import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import com.carrotsearch.junitbenchmarks.annotation.AxisRange;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
 
+// TODO: Auto-generated Javadoc
 /**
  * Tests the number of seconds required to do sequential request/reply cycles,
  * for different possible Actor implementations.
@@ -55,9 +59,94 @@ import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
  *
  * It only tests the fastest implementations.
  */
-@AxisRange(min = 0, max = 3)
+@AxisRange(min = 0, max = 2)
 @BenchmarkMethodChart(filePrefix = "PingPongFastBenchmarks")
+@SuppressWarnings("all")
 public class SmallLatencyBenchmarkTest extends AbstractBenchmark {
+
+    /**
+     * The Class DirectTask.
+     */
+    public class DirectTask extends kilim.Task {
+        SmallLatencyBenchmarkTest target;
+
+        /**
+         * Instantiates a new direct task.
+         *
+         * @param target the target
+         */
+        public DirectTask(final SmallLatencyBenchmarkTest target) {
+            this.target = target;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void execute() throws Pausable, Exception {
+            target.kilim();
+        }
+    }
+
+    /**
+     * The Class ReflectedTask.
+     */
+    public class ReflectedTask extends Task {
+
+        Method method;
+
+        Object target;
+
+        /**
+         * Instantiates a new reflected task.
+         *
+         * @param target the target
+         * @param method the method
+         */
+        public ReflectedTask(final Object target, final Method method) {
+            this.method = method;
+            this.target = target;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void execute() throws Pausable, Exception {
+            ReflectedTask.invoke(method, target);
+        }
+    }
+
+    /**
+     * How many messages to send per test?
+     *
+     * It must be big enough, that the direct impl takes a measurable amount
+     * of time. This means that the slower Actor impl will take each several minutes to test.
+     */
+    protected static final int MESSAGES = 1000000;
+
+    /** The direct task. */
+    private DirectTask directTask;
+
+    /** The reflected task. */
+    private ReflectedTask reflectedTask;
+
+    /**
+     * Test value that is increased each time the {@link #methodCall()}
+    performs one step.
+     */
+    int step;
+
+    /** The ExecutorService */
+    protected ExecutorService executorService;
+
+    /** Factory for JetLang fibers. */
+    protected PoolFiberFactory fiberPool;
+
+    /** The JActor MailboxFactory */
+    protected MailboxFactory jaMailboxFactory;
+
+    /** The PActor Default MailboxFactory */
+    protected DefaultMailboxFactoryImpl paMailboxFactory;
+
+    /** The Akka ActorSystem */
+    protected ActorSystem system;
 
     /** Sets the benchmark properties, for stats and graphics generation. */
     static {
@@ -67,27 +156,22 @@ public class SmallLatencyBenchmarkTest extends AbstractBenchmark {
     }
 
     /**
-     * How many messages to send per test?
+     * Kilim.
      *
-     * It must be big enough, that the direct impl takes a measurable amount
-     * of time. This means that the slower Actor impl will take each several minutes to test.
+     * @throws Pausable the pausable
+     * @throws Exception the exception
      */
-    protected static final int MESSAGES = 1000;
-
-    /** The Akka ActorSystem */
-    protected ActorSystem system;
-
-    /** The JActor MailboxFactory */
-    protected MailboxFactory jaMailboxFactory;
-
-    /** The PActor Default MailboxFactory */
-    protected DefaultMailboxFactoryImpl paMailboxFactory;
-
-    /** The ExecutorService */
-    protected ExecutorService executorService;
-
-    /** Factory for JetLang fibers. */
-    protected PoolFiberFactory fiberPool;
+    public void kilim() throws Pausable, Exception {
+        final kilim.Mailbox<Object> pingerMB = new kilim.Mailbox<Object>();
+        final kilim.Mailbox<Object> pongerMB = new kilim.Mailbox<Object>();
+        final KilimPonger ponger = new KilimPonger(pingerMB, pongerMB);
+        final KilimPinger pinger = new KilimPinger(pingerMB, ponger);
+        final Integer result = pinger.hammer(MESSAGES);
+        if (result.intValue() != MESSAGES) {
+            throw new IllegalStateException("Expected " + MESSAGES
+                    + " but got " + result);
+        }
+    }
 
     /** Setup all "services" for all test methods. */
     @Before
@@ -97,6 +181,20 @@ public class SmallLatencyBenchmarkTest extends AbstractBenchmark {
         jaMailboxFactory = JAMailboxFactory.newMailboxFactory(8);
         paMailboxFactory = new DefaultMailboxFactoryImpl(executorService, false);
         fiberPool = new PoolFiberFactory(executorService);
+
+        try {
+            directTask = new DirectTask(this);
+            final Method kilimMethod = SmallLatencyBenchmarkTest.class
+                    .getMethod("kilim");
+            reflectedTask = new ReflectedTask(this, kilimMethod);
+        } catch (final NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (final SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
     /** Shuts down all "services" for all test methods.
@@ -145,21 +243,6 @@ public class SmallLatencyBenchmarkTest extends AbstractBenchmark {
         }
     }
 
-    /** Test with PActors, by having a reply generate the next request, to eliminate blocking. */
-    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
-    @Test
-    public void testPActorNonBlocking() throws Exception {
-        final PActorNonBlockingPinger pinger = new PActorNonBlockingPinger(
-                paMailboxFactory.createMailbox());
-        final PActorNonBlockingPonger ponger = new PActorNonBlockingPonger(
-                pinger.getMailbox());
-        final Integer result = pinger.hammer(ponger, MESSAGES);
-        if (result.intValue() != MESSAGES) {
-            throw new IllegalStateException("Expected " + MESSAGES
-                    + " but got " + result);
-        }
-    }
-
     /** Test non-blocking JActor simplistic impl, which causes occasional Stack-Overflow! */
     @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
     @Test
@@ -189,19 +272,41 @@ public class SmallLatencyBenchmarkTest extends AbstractBenchmark {
     }
 
     /** Test with Kilim. */
-
     @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
     @Test
-    public void testKilim() throws Pausable, Exception {
-        final kilim.Mailbox<Object> pingerMB = new kilim.Mailbox<Object>();
-        final kilim.Mailbox<Object> pongerMB = new kilim.Mailbox<Object>();
-        final KilimPonger ponger = new KilimPonger(pingerMB, pongerMB);
-        final KilimPinger pinger = new KilimPinger(pingerMB, ponger);
-        final Integer result = pinger.hammer(MESSAGES);
+    public void testKilimDirectTask() {
+        try {
+            directTask._runExecute(null);
+        } catch (final Throwable e) {
+            // TODO Auto-generated catch block
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    /** Test with Kilim. */
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
+    @Test
+    public void testKilimReflectedTask() {
+        try {
+            reflectedTask._runExecute(null);
+        } catch (final Throwable e) {
+            // TODO Auto-generated catch block
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    /** Test with PActors, by having a reply generate the next request, to eliminate blocking. */
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
+    @Test
+    public void testPActorNonBlocking() throws Exception {
+        final PActorNonBlockingPinger pinger = new PActorNonBlockingPinger(
+                paMailboxFactory.createMailbox());
+        final PActorNonBlockingPonger ponger = new PActorNonBlockingPonger(
+                pinger.getMailbox());
+        final Integer result = pinger.hammer(ponger, MESSAGES);
         if (result.intValue() != MESSAGES) {
             throw new IllegalStateException("Expected " + MESSAGES
                     + " but got " + result);
         }
     }
-
 }
